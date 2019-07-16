2c0e02859b9eb5901d5e25ca7f6fdd64cba6efee
Xxtractz <musambaloyi@gmail.com>
2019-07-16 11:02:56 +0200

    ft_ls works

--- only_ft_ls/ft_ls.c
+++ only_ft_ls/ft_ls.c
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../ft_ls.h"
+#include "../includes/ft_ls.h"
+
+int main(){
+    DIR *mydir;
+    struct dirent *files;
+    // struct stat mystat;
+    
+    mydir = opendir(".");
+    while ((files = readdir(mydir)) != NULL)
+    {
+        ft_putstr(files->d_name);
+        ft_putchar('\n');
+    }
+    closedir(mydir);
+    return 0;
+}
